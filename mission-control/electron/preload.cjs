edd22a22f29838b93fb694b92113243e0439f697
Justin Sciaraffa <88063257+Justonejewelry@users.noreply.github.com>
2026-08-25 20:49:04 -0500

    Add Mission Control connection test

--- mission-control/electron/preload.cjs
+++ mission-control/electron/preload.cjs
@@ -3,5 +3,6 @@ contextBridge.exposeInMainWorld('missionControl', {
   health: () => ipcRenderer.invoke('mission:health'),
   git: () => ipcRenderer.invoke('mission:git'),
   diagnostics: () => ipcRenderer.invoke('mission:diagnostics'),
+  ping: () => ipcRenderer.invoke('mission:ping'),
   command: (command) => ipcRenderer.invoke('mission:command', command)
 });
