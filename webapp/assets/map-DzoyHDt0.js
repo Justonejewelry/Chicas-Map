076eed09d1b4ef17dff876bfcc9d5336c51c38d4
Justin Sciaraffa <88063257+Justonejewelry@users.noreply.github.com>
2026-08-27 11:09:05 -0500

    Restore map chunk and make KEY the layer switcher.

--- webapp/assets/map-DzoyHDt0.js
+++ webapp/assets/map-DzoyHDt0.js
@@ -1,1 +1,10 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/leaflet-src-Bv4pEK9w.js","assets/rolldown-runtime-CbXtAM7H.js"])))=>i.map(i=>d[i]);
+/* Restore last-good map chunk. KEY layer toggles live in fs-map.js. */
+const GOOD = "https://cdn.jsdelivr.net/gh/Justonejewelry/Chicas-Map@f3365c745f0a04ef5922ca91b2956684bda3f81e/webapp/assets/map-DzoyHDt0.js";
+const text = (await fetch(GOOD).then((r) => {
+  if (!r.ok) throw new Error("map chunk " + r.status);
+  return r.text();
+}))
+  .replaceAll('from"./",'from"/Chicas-Map/assets/')
+  .replaceAll("import(`./", "import(`/Chicas-Map/assets/");
+const mod = await import(URL.createObjectURL(new Blob([text], { type: "text/javascript" })));
+export const component = mod.component;
