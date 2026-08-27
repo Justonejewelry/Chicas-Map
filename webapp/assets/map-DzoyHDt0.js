afde0aef156f4d11d820f3115d22c394c10129b0
Justin Sciaraffa <88063257+Justonejewelry@users.noreply.github.com>
2026-08-27 11:11:47 -0500

    Fix map chunk restore stub syntax.

--- webapp/assets/map-DzoyHDt0.js
+++ webapp/assets/map-DzoyHDt0.js
@@ -1,10 +1,10 @@
-/* Restore last-good map chunk. KEY layer toggles live in fs-map.js. */
+/* Restore last-good map chunk. KEY layer toggles live in key-layers.js. */
 const GOOD = "https://cdn.jsdelivr.net/gh/Justonejewelry/Chicas-Map@f3365c745f0a04ef5922ca91b2956684bda3f81e/webapp/assets/map-DzoyHDt0.js";
 const text = (await fetch(GOOD).then((r) => {
   if (!r.ok) throw new Error("map chunk " + r.status);
   return r.text();
 }))
-  .replaceAll('from"./",'from"/Chicas-Map/assets/')
-  .replaceAll("import(`./", "import(`/Chicas-Map/assets/");
+  .split('from"./').join('from"/Chicas-Map/assets/')
+  .split("import(`./").join("import(`/Chicas-Map/assets/");
 const mod = await import(URL.createObjectURL(new Blob([text], { type: "text/javascript" })));
 export const component = mod.component;
