d21583788b746a62024c1c5c6a2c783de2c734b0
Justin Sciaraffa <88063257+Justonejewelry@users.noreply.github.com>
2026-08-28 06:01:58 -0500

    Set MapTiler API key

--- webapp/js/chica-config.js
+++ webapp/js/chica-config.js
@@ -4,5 +4,5 @@ window.CHICA_CONFIG = Object.assign({}, window.CHICA_CONFIG, {
   PIN_CLAIM_SKU: "CHICA-PIN-5",
   PIN_CLAIM_ITEM_NAME: "Claimed Pin — Chica’s Map",
   PIN_CLAIM_AFTER_URL: "https://justonejewelry.github.io/Chicas-Map/submit",
-  MAPTILER_KEY: ""
+  MAPTILER_KEY: "ecxzoKzcx8AsCvqSPx3n"
 });
